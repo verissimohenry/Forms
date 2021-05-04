2502606aa0964b23bd028f84dff5663730edb671
karvel-code <velgithinji@gmail.com>
2021-05-04 13:11:36 +0300

    edit new.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def new
-    # @user = User.new
+    @user = User.new
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
     if @user.save
-      raise
-      redirect_to new_user_path
+      redirect_to root_path
     else
-      raise
       render :new
     end
   end
