fd2fb388b4cec4f2481f8cca4573430b7c2a3444
verissimohenry <verissimohenry04@gmail.com>
2021-05-04 10:19:30 +0100

    add new user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,13 +1,24 @@
 class UserController < ApplicationController
-  def new; end
+  def new
+    # @user = User.new
+  end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
     if @user.save
+      raise
       redirect_to new_user_path
     else
+      raise
       render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
